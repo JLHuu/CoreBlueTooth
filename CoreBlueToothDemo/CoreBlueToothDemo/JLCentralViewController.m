dc1d590354dfba18442cc7d935e70b9ee42231fe
JLHuu <953430967@qq.com>
2016-02-25 23:13:01 +0800

    完善

--- CoreBlueToothDemo/CoreBlueToothDemo/JLCentralViewController.m
+++ CoreBlueToothDemo/CoreBlueToothDemo/JLCentralViewController.m
@@ -4,37 +4,85 @@
 //
 //  Created by hujiele on 16/2/25.
 //  Copyright © 2016年 JLHuu. All rights reserved.
-//  中央设备
+//  中央设备使用外围设备的数据。中央设备扫描到外围设备后会就会试图建立连接，一旦连接成功就可以使用这些服务和特征
 
 #import "JLCentralViewController.h"
+#import <CoreBluetooth/CoreBluetooth.h>
 
-@interface JLCentralViewController ()
+@interface JLCentralViewController ()<CBCentralManagerDelegate>//中央设备代理
 - (IBAction)Start:(UIButton *)sender;
 
 @end
 
 @implementation JLCentralViewController
-
+{
+    CBCentralManager *_CentralManager;// 中央设备管理器
+    CBPeripheral *_peripheral;// 外围设备
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+ 
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 - (IBAction)Start:(UIButton *)sender {
+    // 设置代理为self，并将代理回调放在主线程中
+    _CentralManager = [[CBCentralManager alloc] initWithDelegate:self queue:nil];
 }
+#pragma mark - CBCentralManagerDelegate
+// 必须实现的代理方法，中央设备管理器的状态发生改变时调用
+-(void)centralManagerDidUpdateState:(CBCentralManager *)central
+{
+    
+    // 在这里判断CBCentralManager的stutas，并作出相应处理
+    /*
+     stutas为枚举值，有以下几种
+     typedef NS_ENUM(NSInteger, CBCentralManagerState) {
+     CBCentralManagerStateUnknown = 0,
+     CBCentralManagerStateResetting,
+     CBCentralManagerStateUnsupported,
+     CBCentralManagerStateUnauthorized,
+     CBCentralManagerStatePoweredOff,
+     CBCentralManagerStatePoweredOn,
+     };
+     */
+    NSLog(@"%ld",central.state);
+    if (central.state == CBCentralManagerStatePoweredOn) {
+        NSLog(@"蓝牙设备已打开");
+        // 扫描外围设备,serviceUUIDs设为nil会返回所有能扫描到的设备
+        [central scanForPeripheralsWithServices:nil options:@{CBCentralManagerScanOptionAllowDuplicatesKey:@YES}];
+        NSLog(@"正在扫描...");
+    }else{
+        NSLog(@"蓝牙未开启，或不支持");
+    }
+}
+-(void)centralManager:(CBCentralManager *)central willRestoreState:(NSDictionary<NSString *,id> *)dict
+{
+    NSLog(@"RestoreState%@",dict);
+}
+// 发现外围设备
+-(void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary<NSString *,id> *)advertisementData RSSI:(NSNumber *)RSSI
+{
+    NSLog(@"发现外围设备");
+    // 停止扫描
+    [central stopScan];
+    NSLog(@"扫描停止");
+    // 与外围设备建立连接
+    [central connectPeripheral:peripheral options:@{CBConnectPeripheralOptionNotifyOnConnectionKey:@YES}];
+}
+// 连接上外围设备
+-(void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral
+{
+    NSLog(@"连接上外围设备");
+}
+// 丢失与外围设备的连接
+-(void)centralManager:(CBCentralManager *)central didDisconnectPeripheral:(CBPeripheral *)peripheral error:(NSError *)error
+{
+    NSLog(@"丢失连接");
+}
+// 连接外围设备失败
+-(void)centralManager:(CBCentralManager *)central didFailToConnectPeripheral:(CBPeripheral *)peripheral error:(NSError *)error
+{
+    NSLog(@"连接失败,%@",error);
+}
+
 @end
--- CoreBlueToothDemo/CoreBlueToothDemo/JLPeripheralViewController.m
+++ CoreBlueToothDemo/CoreBlueToothDemo/JLPeripheralViewController.m
@@ -4,37 +4,116 @@
 //
 //  Created by hujiele on 16/2/25.
 //  Copyright © 2016年 JLHuu. All rights reserved.
-//  外围设备
+//  外围设备通常用于发布服务、生成数据、保存数据。外围设备发布并广播服务，告诉周围的中央设备它的可用服务和特征
 
 #import "JLPeripheralViewController.h"
+#import <CoreBluetooth/CoreBluetooth.h>
 
-@interface JLPeripheralViewController ()
+#define ServiceUUID @"A48D3B5C-353D-4093-8821-DDDEBFEFEA32"
+#define CharacteristicUUID @"6A3E4B28-522D-4B3B-82A9-D5E2004534FC"
+
+@interface JLPeripheralViewController ()<CBPeripheralManagerDelegate>
 - (IBAction)Start:(UIButton *)sender;
+- (IBAction)Send:(UIButton *)sender;
 
 @end
 
 @implementation JLPeripheralViewController
-
+{
+    CBPeripheralManager *_PeripheralManager;// 外围设备管理器
+    CBMutableCharacteristic *_PerCharacteristic; // 特征值
+    NSMutableArray <CBCentral *>*_centrals;// 存储订阅特征的中心设备
+    
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    _centrals = [NSMutableArray array];
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 - (IBAction)Start:(UIButton *)sender {
+    NSLog(@"1111");
+    _PeripheralManager = [[CBPeripheralManager alloc] initWithDelegate:self queue:nil];
+}
+
+- (IBAction)Send:(UIButton *)sender {
+    [self _updateCharacteristicValue];
+}
+#pragma mark - CBPeripheralManagerDelegate
+// 外围设备状态发生变化
+-(void)peripheralManagerDidUpdateState:(CBPeripheralManager *)peripheral
+{
+    NSLog(@"%ld",peripheral.state);
+    if (peripheral.state == CBPeripheralManagerStatePoweredOn) {
+        NSLog(@"蓝牙打开");
+        // 添加服务
+        [self _addService];
+    }else{
+        NSLog(@"蓝牙未开启，或不支持");
+    }
+}
+// 添加服务后调用
+-(void)peripheralManager:(CBPeripheralManager *)peripheral didAddService:(CBService *)service error:(NSError *)error
+{
+    if (!error) {
+        NSLog(@"添加服务成功");
+        // 服务添加后开始广播
+        [peripheral startAdvertising:@{CBAdvertisementDataLocalNameKey:@"XIAOLE's Iphone"}];
+    }else{
+        NSLog(@"添加服务失败error：%@",error);
+    }
+}
+-(void)peripheralManagerDidStartAdvertising:(CBPeripheralManager *)peripheral error:(NSError *)error
+{
+    if (!error) {
+        NSLog(@"服务启动");
+    }else{
+        NSLog(@"服务启动失败:%@",error);
+    }
+}
+// 中心设备订阅特征调用
+-(void)peripheralManager:(CBPeripheralManager *)peripheral central:(CBCentral *)central didSubscribeToCharacteristic:(CBCharacteristic *)characteristic
+{
+    NSLog(@"中心设备订阅特征central:%@,characteristic:%@",central,characteristic);
+    if (central) {
+        // 保存中心设备
+        [_centrals addObject:central];
+    }
+}
+// 中心设备取消订阅特征
+- (void)peripheralManager:(CBPeripheralManager *)peripheral central:(CBCentral *)central didUnsubscribeFromCharacteristic:(CBCharacteristic *)characteristic
+{
+    NSLog(@"中心设备取消订阅特征:central%@,characteristic:%@",central,characteristic);
+}
+-(void)peripheralManager:(CBPeripheralManager *)peripheral didReceiveWriteRequests:(CBATTRequest *)request{
+    NSLog(@"didReceiveWriteRequests");
+}
+-(void)peripheralManager:(CBPeripheralManager *)peripheral willRestoreState:(NSDictionary *)dic{
+    NSLog(@"willRestoreState");
+}
+// 更新特征时调用
+-(void)peripheralManagerIsReadyToUpdateSubscribers:(CBPeripheralManager *)peripheral
+{
+    NSLog(@"ReadyToUpdateSubscribers");
+}
+#pragma mark - -
+- (void)_addService
+{
+    NSLog(@"添加服务");
+    // 特征
+    _PerCharacteristic = [[CBMutableCharacteristic alloc] initWithType:[CBUUID UUIDWithString:CharacteristicUUID] properties:CBCharacteristicPropertyNotify | CBCharacteristicPropertyRead |CBCharacteristicPropertyWrite value:nil permissions:CBAttributePermissionsWriteable];
+    // 创建服务
+    CBMutableService *service = [[CBMutableService alloc] initWithType:[CBUUID UUIDWithString:ServiceUUID] primary:YES];
+    // 设置服务的特征
+    [service setCharacteristics:@[_PerCharacteristic]];
+    // 外围设备添加服务
+    [_PeripheralManager addService:service];
+}
+
+- (void)_updateCharacteristicValue
+{
+    NSLog(@"更新特征值");
+    // 更新特征值
+    NSString *str = [@"特征值:" stringByAppendingString:[NSString stringWithFormat:@"%@",[NSDate date]]];
+    [_PeripheralManager updateValue:[str dataUsingEncoding:NSUTF8StringEncoding]  forCharacteristic:_PerCharacteristic onSubscribedCentrals:_centrals];
 }
 @end
